64b24d8240d4909f00630252885e7bfbb6330538
hosie <jhosie@uk.ibm.com>
2016-05-15 08:30:09 +0100

    style

--- src/public/javascripts/ExercisesController.js
+++ src/public/javascripts/ExercisesController.js
@@ -1,42 +1,42 @@
 /*
-Copyright 2015 
-Author John Hosie 
- 
-The MIT License (MIT) 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+ Copyright 2015
+ Author John Hosie
 
- 
-  Contributors:
-      John Hosie - initial implementation 
-*/
+ The MIT License (MIT)
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
 
-var fitBuddyApp = angular.module('FitBuddyApp',[]);
 
-fitBuddyApp.controller('ExercisesController',function($scope,$http){
+ Contributors:
+ John Hosie - initial implementation
+ */
 
-    
-    $http.get('exercises').success(function(data){
-        $scope.exercises=data;
+var fitBuddyApp = angular.module('FitBuddyApp', []);
+
+fitBuddyApp.controller('ExercisesController', function ($scope, $http) {
+
+
+    $http.get('exercises').success(function (data) {
+        $scope.exercises = data;
     });
 
-    $scope.update=function(exercise){
+    $scope.update = function (exercise) {
         console.log("posting" + exercise);
-        $http.post('exercises',exercise);
+        $http.post('exercises', exercise);
     };
 
-    $scope.create=function(name){
+    $scope.create = function (name) {
         console.log("new " + name);
         var newExercise = {
-            "name":name
+            "name": name
         };
         var newExerciseString = JSON.stringify(newExercise);
         console.log("posting " + newExerciseString);
-        $http.post('exercises',newExerciseString).success(function(data){
+        $http.post('exercises', newExerciseString).success(function (data) {
             console.log("got POST response" + data);
             $scope.exercises.push(newExercise);
 
