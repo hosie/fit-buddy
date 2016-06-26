fbcda266486ba422d8f0a5fc3ac16c74f4743fab
hosie <jhosie@uk.ibm.com>
2016-06-26 21:27:32 +0100

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var Server = require('karma').Server;
+var istanbul = require('gulp-istanbul');
+
+var paths = {
+    serverCode: ['app.js','exercises/**/*.js','persistence/**/*.js'],
+    serverTests: ['test/server/**/*.js'],
+    clientCode: 'client/img/**/*'
+};
+
+gulp.task('default', function() {
+    // place code for your default task here
+});
+
+gulp.task('test', ['server-test', 'client-test'], function() {
+
+});
+
+gulp.task('pre-server-test', function () {
+    return gulp.src(paths.serverTests.concat(paths.serverCode))
+    // Covering files
+    .pipe(istanbul())
+    // Force `require` to return covered files
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task('server-test', ['pre-server-test'], function() {
+    return gulp.src(paths.serverTests, {read: false})
+    .pipe(mocha())
+    .pipe(istanbul.writeReports({
+        dir: './coverage/server',
+        reporters: ['cobertura','html']
+    }))
+    // Enforce a coverage of at least 90%
+    .pipe(istanbul.enforceThresholds({ thresholds: { global: 10 } }));
+});
+
+gulp.task('client-test', ['server-test'], function(done) {
+    new Server({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: true
+    }, done).start();
+});
--- karma.conf.js
+++ karma.conf.js
@@ -29,13 +29,17 @@ module.exports = function(config) {
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
+            // source files, that you wanna generate coverage for
+            // do not include tests or libraries
+            // (these files will be instrumented by Istanbul)
+            'public/javascripts/**/*.js': ['coverage']
         },
 
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters: ['progress','coverage'],
 
 
         // web server port
@@ -58,6 +62,11 @@ module.exports = function(config) {
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         browsers: ['Chrome'],
+
+        coverageReporter: {
+            dir: 'coverage/client',
+            subdir: '.'
+        },
 
 
         // Continuous Integration mode
